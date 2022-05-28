5be640f10de6c3d78aaa655c3bc6ca52f041ca07
oswaldobenal <oswaldobenal12@gmail.com>
2022-05-27 21:50:03 -0500

    Autenticacion de usuarios completado

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,8 +1,45 @@
-export const register = (req, res) => {
-  console.log(req.body);
-  res.json({ ok: true });
+import { User } from "../models/User.js";
+import jwt from "jsonwebtoken";
+
+export const register = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    //opcion 2 Alternativa buscando por email
+    let user = await User.findOne({ email });
+    if (user) throw { code: 11000 };
+
+    user = new User({ email, password });
+    await user.save();
+
+    // Generar el token JWT
+
+    return res.status(201).json({ ok: true });
+  } catch (error) {
+    console.log(error.code);
+    //Alternativa por defecto mongoose
+    if (error.code === 11000) {
+      res.status(400).json({ error: "Ya existe este usuario" });
+    }
+    return res.status(500).json({ error: "Error de servidor" });
+  }
 };
 
-export const login = (req, res) => {
-  res.json({ ok: true });
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    let user = await User.findOne({ email });
+    if (!user) return res.status(403).json({ error: "No existe el usuario" });
+
+    const respuestaPassword = await user.comparePassword(password);
+    if (!respuestaPassword)
+      return res.status(403).json({ error: "Password Incorrecta" });
+
+    //Generar el token JWT
+    const token = jwt.sign({ uid: user.id }, process.env.JWT_SECRET);
+
+    return res.json({ token });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ error: "Error de servidor" });
+  }
 };
--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import authRouter from './routes/auth.route.js'
 const app = express();
 
 app.use(express.json()); // middleware para leer el body.
-app.use('/api/v1', authRouter)
+app.use('/api/v1/auth', authRouter)
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log("🔥🔥🔥 http://localhost:5000"));
+app.listen(PORT, () => console.log("🔥🔥🔥 http://localhost:"+ PORT));
